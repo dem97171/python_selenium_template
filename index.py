69d7e6f412f7ab90c6d1cfe64f3dccd95801f6dc
wtnb9434 <dem97171wtnb@gmail.com>
2018-10-28 16:44:47 +0900

    challenge login.

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
+import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
+dir_name = "./images/"
+
 def main():
-    url = "https://s-portal.cloud.global.fujitsu.com/SK5PCOM001/"
+    _url = "https://s-portal.cloud.global.fujitsu.com/SK5PCOM001/"
 
     # プラウザ起動（Chrome）
     options = Options()
@@ -16,9 +19,8 @@ def main():
     driver = webdriver.Chrome(executable_path="./vendor/driver/chromedriver", chrome_options=options,
                               service_args=["--debug", "--log-path=./logs/debug.log"])
 
-    # 作業topページを開く。
-    dir_name = "./images/"
-    driver.get(url)  # 画面get
+    # 作業topページを開く
+    driver.get(_url)  # 画面get
     time.sleep(2)  # 読み込み待機時間
     driver.save_screenshot(dir_name + "01_top.png")  # imagesフォルダにスクリーンショットを保存
 
@@ -27,8 +29,28 @@ def main():
     time.sleep(2)
     driver.save_screenshot(dir_name + "02_login.png")  # imagesフォルダにスクリーンショットを保存
 
-    # プラウザを閉じる
-    driver.quit()
+    # Loginフォームに値を入力してログインボタンをクリック
+    driver.find_element_by_id("keiyakuno").send_keys("12345678")
+    driver.find_element_by_id("username").send_keys("username")
+    driver.find_element_by_id("password").send_keys("password")
+    driver.save_screenshot(dir_name + "03_login_input.png")  # imagesフォルダにスクリーンショットを保存
+
+    driver.find_element_by_id("Submit").click()
+    time.sleep(2)
+    driver.save_screenshot(dir_name + "04_login_result.png")  # imagesフォルダにスクリーンショットを保存
+
+    _unExpectedUrl = "https://k5-authentication.paas.cloud.global.fujitsu.com/cloudlink/module.php/core/loginuserpass.php?"
+    if driver.current_url == _unExpectedUrl:
+        closeSelenium(driver)   # ログインに失敗したらselenium終了
+
+
+    closeSelenium(driver)
+
+
+def closeSelenium(_driver):
+    _driver.save_screenshot(dir_name + "05_login_result.png")  # imagesフォルダにスクリーンショットを保存
+    _driver.quit()
+    sys.exit()
 
 
 main()
